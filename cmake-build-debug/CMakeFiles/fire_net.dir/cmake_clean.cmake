8fb94349e49132e4e268cf30876b53231bdb901d
Key <75124495+XWQ126@users.noreply.github.com>
2021-07-09 15:12:58 +0800

    第一次提交(部分代码，未完善)

---
+++ cmake-build-debug/CMakeFiles/fire_net.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fire_net.dir/main.cpp.obj"
+  "fire_net.exe"
+  "fire_net.exe.manifest"
+  "fire_net.pdb"
+  "libfire_net.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/fire_net.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+int n;
+char map[100][100];
+int main() {
+    int max_num=0;  //记录每一次查找到的最大值
+    int max_sum=0;  //用来比较每次查找的最大值的其中的最大值
+    int sum=0;  //用来记录当前找到的已经放置的炮楼的数量
+    int flag=0; //当作标记，判断当前该位置是否放置炮楼
+
+
+}
